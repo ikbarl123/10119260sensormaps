7fb193ee17662ae843237dc8e870ef0d004add8f
ikbar <ikbarl123@gmail.com>
2022-07-01 10:55:20 +0700

    user location

--- app/src/main/java/com/alimuntung/a10119260sensormaps/Presenter/Maps/MapsFragment.java
+++ app/src/main/java/com/alimuntung/a10119260sensormaps/Presenter/Maps/MapsFragment.java
@@ -5,14 +5,17 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import android.app.AlertDialog;
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alimuntung.a10119260sensormaps.Model.RestaurantMarker;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,11 +23,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.alimuntung.a10119260sensormaps.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapsFragment extends Fragment {
+    private FusedLocationProviderClient mFusedclient;
+    private SupportMapFragment mapFragment;
+    Location lokasi;
     private List<RestaurantMarker> restaurantMarkerList= new ArrayList<RestaurantMarker>();
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
@@ -36,8 +44,9 @@ public class MapsFragment extends Fragment {
             restaurantMarkerList.add(new RestaurantMarker(-6.934462231054307, 107.62553287262733,"Gokana Ramen"));
             restaurantMarkerList.add(new RestaurantMarker(-6.93392438790206, 107.6237089705169,"Nasi Tim Ayam Sizi"));
             restaurantMarkerList.add(new RestaurantMarker(-6.9350373497852615, 107.62436879392745,"Rumah Makan Mergosari"));
-            LatLng bandung = new LatLng(-6.9301087,107.617305);
+            //LatLng bandung = new LatLng(-6.9301087,107.617305);
             //loop marker
+            LatLng curcloc = new LatLng(lokasi.getLatitude(),lokasi.getLongitude());
 
             for (int i=0; i<restaurantMarkerList.size();i++){
                 googleMap.addMarker(new MarkerOptions().position(
@@ -46,9 +55,9 @@ public class MapsFragment extends Fragment {
                                 restaurantMarkerList.get(i).getLng()
                         )).title(restaurantMarkerList.get(i).getTitle()));
             }
-          //  googleMap.addMarker(new MarkerOptions().position(bandung).title("Marker in Bandung"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bandung,15f));
-            //googleMap.moveCamera(CameraUpdateFactory.);
+           googleMap.addMarker(new MarkerOptions().position(curcloc).title("Lokasi Anda Sekarang"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curcloc,15f));
+
         }
     };
 
@@ -57,18 +66,41 @@ public class MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+      View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
+        mFusedclient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            getLocation();
+//            mapFragment.getMapAsync(callback);
         }
+
+
     }
 
 
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+            Task<Location> task = mFusedclient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location locationnow) {
+                    if(locationnow != null){
+                        lokasi = locationnow;
+                        mapFragment.getMapAsync(callback);
+                    }
+                }
+            });
+
+    }
 }
+
