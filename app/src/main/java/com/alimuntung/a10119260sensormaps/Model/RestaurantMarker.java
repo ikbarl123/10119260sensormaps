e759de7d48f00bb318a5aa4ee85dcd9804da9fde
ikbar <ikbarl123@gmail.com>
2022-07-01 09:41:57 +0700

    marker model + marker loop

---
+++ app/src/main/java/com/alimuntung/a10119260sensormaps/Model/RestaurantMarker.java
@@ -0,0 +1,24 @@
+package com.alimuntung.a10119260sensormaps.Model;
+
+public class RestaurantMarker {
+    private double lat;
+    private double lng;
+    private String title;
+
+    public RestaurantMarker(double lat, double lng, String title) {
+        this.lat = lat;
+        this.lng = lng;
+        this.title = title;
+    }
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- app/src/main/java/com/alimuntung/a10119260sensormaps/Presenter/Maps/MapsFragment.java
+++ app/src/main/java/com/alimuntung/a10119260sensormaps/Presenter/Maps/MapsFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alimuntung.a10119260sensormaps.Model.RestaurantMarker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,15 +21,33 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.alimuntung.a10119260sensormaps.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsFragment extends Fragment {
+    private List<RestaurantMarker> restaurantMarkerList= new ArrayList<RestaurantMarker>();
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng bandung = new LatLng(-6.9101087,107.617305);
-            googleMap.addMarker(new MarkerOptions().position(bandung).title("Marker in Bandung"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bandung,12f));
+            restaurantMarkerList.add(new RestaurantMarker(-6.931916789683658, 107.62516272779035,"Baso Akung"));
+            restaurantMarkerList.add(new RestaurantMarker(-6.931765021303783, 107.62469063320991,"Bandung Cheesecuit Lodaya"));
+            restaurantMarkerList.add(new RestaurantMarker(-6.934462231054307, 107.62553287262733,"Gokana Ramen"));
+            restaurantMarkerList.add(new RestaurantMarker(-6.93392438790206, 107.6237089705169,"Nasi Tim Ayam Sizi"));
+            restaurantMarkerList.add(new RestaurantMarker(-6.9350373497852615, 107.62436879392745,"Rumah Makan Mergosari"));
+            LatLng bandung = new LatLng(-6.9301087,107.617305);
+            //loop marker
+
+            for (int i=0; i<restaurantMarkerList.size();i++){
+                googleMap.addMarker(new MarkerOptions().position(
+                        new LatLng(
+                                restaurantMarkerList.get(i).getLat(),
+                                restaurantMarkerList.get(i).getLng()
+                        )).title(restaurantMarkerList.get(i).getTitle()));
+            }
+          //  googleMap.addMarker(new MarkerOptions().position(bandung).title("Marker in Bandung"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bandung,15f));
             //googleMap.moveCamera(CameraUpdateFactory.);
         }
     };
